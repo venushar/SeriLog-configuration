def0912f64181c7bf5799842a1f78fff02889aea
venushar <vrajapaksha@outlook.com>
2020-11-08 17:32:35 +0530

    initial commit

---
+++ serilog/WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+       
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("Get method");
+
+
+            int count;
+            try
+            {
+                for (count = 0; count <= 5; count++)
+                {
+                    if (count == 3)
+                    {
+                        throw new Exception("RandomException");
+                    }
+                }
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Exception Caught");
+                return BadRequest();
+            }
+        }
+    }
+}
+
